9441ccbdc18996c5cc626489a613baccff969dbb
pknowledge <sdaqit96@gmail.com>
2022-05-12 00:46:06 +0300

    Day 1 Tasks

---
+++ Day-1/Main.java
@@ -0,0 +1,20 @@
+package Day1;
+
+public class Main {
+    public static void main(String[] args) {
+        byte date = 9;
+        int month = 5;
+        long year = 2022l;
+        char day = 'M';
+        float time = 4.45f;
+        double classEndTime = 5.00d;
+        System.out.println("Boot Camp Task");
+        System.out.printf("Date in Byte Type: ", date);
+        System.out.println("Month in Integar Type: "+month);
+        System.out.println("Year in Long Type: "+year);
+        System.out.println("Day in Character Type: "+day);
+        System.out.println("Time in Float Type: "+time);
+        System.out.println("Class End Time in Doulbe"+classEndTime);
+    }
+
+}
