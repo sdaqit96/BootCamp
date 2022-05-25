2b83e251065de22b0fc60b2c233898919e3e6835
pknowledge <sdaqit96@gmail.com>
2022-05-25 16:36:22 +0300

    StSystem Project Part

---
+++ stsystem/src/main/java/com/Stsystem/stsystem/StsystemApplication.java
@@ -0,0 +1,13 @@
+package com.Stsystem.stsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsystemApplication.class, args);
+	}
+
+}
---
+++ stsystem/src/test/java/com/Stsystem/stsystem/StsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Stsystem.stsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
